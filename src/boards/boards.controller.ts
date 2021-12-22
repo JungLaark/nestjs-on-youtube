18fd88ee4dcc2b5f71ae5d8a2e7a0686de141eaa
JCS <chenone839@gmail.com>
2021-12-22 17:26:01 +0900

    20211222

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardService: BoardService) {}
+    constructor(private boardsService: BoardsService) {}
+
+    //경로가 생략되어있으면 "/"" 이다.
+    @Get()
+    getAllBoard(): Board[] {
+        return this.boardsService.getAllBoard();
+    }
+
+    @Post()
+    createBoard(@Body('title') title: string, @Body('description') description: string): Board {
+        return this.boardsService.createBoard(title, description);
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    //다른 컴포넌트에서의 수정 방지 위함
+    private boards: Board[] = [];
+
+    //:Board[] 리턴값의 자료형 정의
+    getAllBoard(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        };
+
+        this.boards.push(board);
+
+        return board;
+    }
+}
